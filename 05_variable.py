687c02124a97ebdda01fba22ead332852d57c768
awaisashfaq194 <awaisashfaq194@gmail.com>
2022-01-09 15:42:27 +0500

    first

---
+++ 05_variable.py
@@ -0,0 +1,28 @@
+# Variables: objects containing specific values
+x=5 #numeric variable
+print(x)
+y="we are learning python with ammar"  #string variable
+print(y)
+x=15
+print(x)
+z=1
+a=x+z
+print(a)
+#types of variables
+type(x)
+print(type(x))
+print(type(y))
+# Rules to assign a variable
+# 1 The variable should contains letters, numbers or underscores
+# 2 Dont start with numbers
+# 3 Spaces are not allowed 
+# 4 Dont use keywords used in function(break, mean, media, test etc)
+# 5 Short and descriptive
+# 6 Case sensitivity (Lowercase, Upercase letter. lowercase letter should be used)
+
+
+fruit_basket="Mangoes , Oranges"
+del fruit_basket
+print(fruit_basket)
+type(fruit_basket)
+print(type(fruit_basket))
